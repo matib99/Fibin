4228e9a7856274588ede1c99a7ad54e6cd7e31fa
matib99 <40698588+matib99@users.noreply.github.com>
2019-12-01 23:43:19 +0100

    Add files via upload

--- fibin.h
+++ fibin.h
@@ -52,6 +52,34 @@ public:
     template <typename T1, typename T2>
     struct Eq {};
 
+    template <typename Arg1, typename Arg2, typename... Args>
+    struct Sum {};
+
+    template <const char* VariableName>
+    struct Var
+    {
+        const char* name = VariableName;
+    };
+
+    template <typename T> // tu jakiś błąd trzeba jeśli damy coś innego od Var, albo to jakoś lepiej rozwiązać
+    struct Ref{};
+
+    template <const char* Name>
+    struct Ref<Var<Name>>{};
+
+
+    template <typename Variable, typename Value, typename Expression> // jak wyżej
+    struct Let {};
+
+    template <const char* Name, typename Value, typename Expression>
+    struct Let<Var<Name>, Value, Expression> {};
+
+    template<class C>
+    static void print() {
+        std::cout << C::val << "\n";
+    }
+
+
 
 private:
 
@@ -99,13 +127,6 @@ private:
         const static auto val = T::val;
     };
 
-    //Eq<T1,T2>
-    template <typename T1, typename T2, typename Env>
-    struct Eval<Eq<T1, T2>, Env>
-    {
-        const static bool val = Eval<T1, Env>::val == Eval<T2, Env>::val;
-    };
-
     //Inc1<Arg>
     template <typename Arg, typename Env>
     struct Eval<Inc1<Arg>, Env>
@@ -120,11 +141,62 @@ private:
         const static ValueType val = Eval<Arg, Env>::val + Eval<Lit<Fib<10>>, Env>::val;
     };
 
+    //Sum<Arg> |Args| = 2
+    template <typename Arg1, typename Arg2, typename Env>
+    struct Eval<Sum<Arg1, Arg2>, Env>
+    {
+        const static ValueType val = Eval<Arg1, Env>::val + Eval<Arg2, Env>::val;
+    };
 
-    template<class C>
-    static void print() {
-        std::cout << C::val << "\n";
-    }
+    //Sum<Args> |Args| > 2
+    template <typename Arg1, typename Arg2, , typename... Args, typename Env>
+    struct Eval<Sum<Arg1, Arg2, Args...>, Env>
+    {
+        const static ValueType val = Eval<Arg1, Env>::val + Eval<Sum<Arg2, Args...>, Env>::val;
+    };
+
+    //prawdopodobnie trzeba będzie zrobić jakiś error kiedy argumentów w sum będzie mniej niż 2
+
+    //Eq<T1,T2>
+    template <typename T1, typename T2, typename Env>
+    struct Eval<Eq<T1, T2>, Env>
+    {
+        const static bool val = Eval<T1, Env>::val == Eval<T2, Env>::val;
+    };
+
+    template <const char* Name, typename Env>
+    struct Eval<Ref<Var<Name>>, Env>
+    {
+        // tutaj pewien problem, bo to może zwracać też typ. więc nie wiem. Trzeba bedzie typedefować val,
+        // ale z drugiej strony co wtedy robić kiedy wartościąjest po prostu
+
+        // możliwe, że trzeba będzie to wszystko przebudować :c
+
+        // albo jawnie zrobić 2 rodzaje jakby Variabli gdzie jedna jest funkcjąa druga wyrażeniem, które oblicza siędo wartości.
+        // i wtedy, w invoke np dać 2 opcje, gdy pierwszy argument to Ref<Var> lub gdy pierwszy argument to Lambda
+        // (ale to by było brzydsze)
+    };
+
+    template<const char* Name, typename Value, typename Expression, typename Env>
+    struct Eval<Let<Var<Name>, Value, Expression>, Env>
+    {
+        // wykonujemy Expression ale w środowisku które zawiera nową zmienną
+        const static auto val = Eval<Expression, Binding<Name, Value, Env>>::val;
+    };
+
+
+    /*
+     * TODO:
+     * - IF
+     * - Lambda
+     * - Invoke
+     * - pomyśleć co z tymi Refami jak to rozwiązać
+     * - napisać eval(), co nie powinno byćproblemem, skoro Eval robi wszystko
+     *      (Eval jest po to, żeby ukryć Env których nie chcą w specyfikacji)
+     * - Testy...
+    */
+
+
 };
 
 #endif //FIBIN_FIBIN_H
